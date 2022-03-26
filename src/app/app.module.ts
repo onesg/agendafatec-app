1465c4fb646e03933cee5e31e44122b184e06909
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-26 14:05:49 -0300

    Formulario Contato foi criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 //  importações manuais (colocar entre as importações existentes)
 import { MatButtonModule } from '@angular/material/button';
 import { ContatoService } from './contato.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +23,8 @@ import { ContatoComponent } from './contato/contato.component';
 
     //  colocar antes do BrowserAnimationsModule
     MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
 
     BrowserAnimationsModule
   ],
