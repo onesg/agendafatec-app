4121478e13818811195dc57252793c6c8b0e583b
onesg <gabriel.seno@fatec.sp.gov.br>
2022-04-25 22:11:35 -0300

    Começando a implementar componente de ContatoDetalhe

--- src/app/contato-detalhe/contato-detalhe.component.ts
+++ src/app/contato-detalhe/contato-detalhe.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
+import { Contato } from '../contato/contato';
 
 @Component({
   selector: 'app-contato-detalhe',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoDetalheComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+
+    /* Injectando os dados do contato em uma caixa de dialogo */
+    public dialogRef: MatDialogRef<ContatoDetalheComponent>,
+    @Inject(MAT_DIALOG_DATA) public contato: Contato
+
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  /* MÉTODO PARA FECHAR A CAIXA DE DIALOGO */
+  closeWindow(){
+    this.dialogRef.close();
   }
 
 }
